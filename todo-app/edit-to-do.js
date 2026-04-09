5f6d2bc3a08af4faa0901f2b9579e936914ebeae
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2026-04-09 16:15:18 -0700

    Refactor editToDo function and add new functions

--- todo-app/edit-to-do.js
+++ todo-app/edit-to-do.js
@@ -1,3 +1,5 @@
+import { createEditForm, createProjectForm, addToDoForm } from './dom.js';
+
 export function editToDo() {
     const editBtn = document.querySelector('.edit-to-do');
     editBtn.addEventListener('click', () => {
@@ -6,11 +8,28 @@ export function editToDo() {
     });
 }
 
-function createEditForm() {
-    const toDoTitle = document.querySelector('.to-do-title');
-    const toDoDescription = document.querySelector('.to-do-description');
-    let title = prompt('Edit Title:');
-    let description = prompt('Project Description:');
-    toDoTitle.textContent = title;
-    toDoDescription.textContent = description;
+export function createTodoList(title, description, date, task) {
+    let list = [];
+    return {
+        title: list.push(title),
+        description: list.push(description),
+        date: list.push(date),
+        task: list.push(task)
+    }
+}
+
+export function addProject() {
+    const addProjectBtn = document.querySelector('.add-project');
+    addProjectBtn.addEventListener('click', () => {
+        console.log('Adding new project');
+        createProjectForm();
+    });
+}
+
+export function addTodo() {
+    const newTodoBtn = document.querySelector('.add-to-do');
+    newTodoBtn.addEventListener('click', () => {
+        console.log('Adding new Todo');
+        addToDoForm();
+    });
 }
